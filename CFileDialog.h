c53c8ef277fb7f12b9bc332019e98087fc557fb6
monoliths-uni <monoliths@sina.com>
2021-08-07 20:35:55 +0800

    solve error git

---
+++ CFileDialog.h
@@ -0,0 +1,15 @@
+#ifndef CFILEDIALOG_H
+#define CFILEDIALOG_H
+
+#include <QObject>
+#include <QFileDialog>
+
+class CFileDialog : public QFileDialog
+{
+    Q_OBJECT
+    public:
+        CFileDialog(QWidget* parent = 0);
+
+};
+
+#endif
